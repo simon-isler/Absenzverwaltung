f435231aecd3c92c30d4dde6f80fb593a7a37e30
simon-isler <simon-isler@gmx.ch>
2018-11-09 09:57:14 +0100

    datepicker

---
+++ js/datepicker/ownDatepicker.js
@@ -0,0 +1,8 @@
+$('.datepicker').datepicker({
+    weekStart: 1,
+    todayBtn: true,
+    language: "de",
+    daysOfWeekDisabled: "0,2,3,4,6",
+    daysOfWeekHighlighted: "1,5",
+    calendarWeeks: true
+});
